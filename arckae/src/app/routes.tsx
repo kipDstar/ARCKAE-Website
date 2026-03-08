02d1b05f9fd99ab96c901caaa031309b51dcc5bc
kipsta <kipsangjesse17@gmail.com>
2026-03-08 20:39:02 +0300

    Clean up project structure: remove duplicate folders, rename figma-design to frontend

--- arckae/src/app/routes.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-// Example route configuration; you can integrate react-router-dom later
-
-export const routes = [
-  { path: '/', component: React.lazy(() => import('./pages/Home')) },
-  { path: '/about', component: React.lazy(() => import('./pages/About')) },
-];
