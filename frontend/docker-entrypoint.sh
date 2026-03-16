9137e76aad1edb0d8239e9e049473ed68f2f0dec
kipsta <kipsangjesse17@gmail.com>
2026-03-16 21:42:08 +0300

    Update backend environment variable in docker-entrypoint.sh to improve service configuration

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
 # Substitute BACKEND_HOSTPORT into nginx config (Render sets this via fromService; locally use backend:8000)
-export BACKEND_HOSTPORT="${BACKEND_ORIGIN:-backend:8000}"
+export BACKEND_ORIGIN="${BACKEND_ORIGIN:-http://backend:8000}"
 envsubst '${BACKEND_ORIGIN}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 exec nginx -g "daemon off;"
