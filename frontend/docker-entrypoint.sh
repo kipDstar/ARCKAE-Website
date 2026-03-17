31a62c11b9aba017653fb23ed52c2c57c724de5e
kipsta <kipsangjesse17@gmail.com>
2026-03-18 00:24:42 +0300

    Enhance Nginx and entrypoint configurations for improved backend integration
    - Added logging of the injected backend host in docker-entrypoint.sh for better visibility.
    - Updated Nginx configuration to use safer syntax for environment variable substitution in proxy_pass directive.

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -4,5 +4,7 @@ set -e
 # Replace $BACKEND_HOSTPORT in the template and output to the real config file
 envsubst '$BACKEND_HOSTPORT' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 
+echo "Injected Backend Host: $BACKEND_HOSTPORT"
+
 # Start Nginx
 exec nginx -g 'daemon off;'
